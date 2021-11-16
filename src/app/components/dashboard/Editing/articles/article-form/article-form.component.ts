3cd8e12c942b3bba307d975f7f434c671b5f4ba8
Lisitius <90479443+Lisitius@users.noreply.github.com>
2021-11-16 12:01:07 +0100

    16/11/-2021 à 12h00
    Article non terminée
    16/11/-2021 à 12h00

--- src/app/components/dashboard/Editing/articles/article-form/article-form.component.ts
+++ src/app/components/dashboard/Editing/articles/article-form/article-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Article } from 'src/app/models/models';
+import { AdminLoginService } from 'src/app/services/Admin/admin-login.service';
+import { ArticleService } from 'src/app/services/Admin/Editing/article.service';
 
 @Component({
   selector: 'app-article-form',
@@ -6,8 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-form.component.scss']
 })
 export class ArticleFormComponent implements OnInit {
+  id:string ='';
 
-  constructor() { }
+  article: Article = {
+    id: '',
+    titre: '',
+    image: '',
+    contenu: '',
+    payload: undefined
+  }
+
+  constructor(
+    public articleservice: ArticleService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private admin: AdminLoginService
+  ) {}
+
+  save(articleForm: NgForm) {
+    if(articleForm.valid) {
+      if (this.id) {
+        this.articleservice.updateArticle(articleForm.value, this.id);
+        this.admin.showNotification('Article modifié !');
+      } else {
+        this.articleservice.addArticle(articleForm.value);
+        this.admin.showNotification("L'article à été créé")
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/dashboard/Editing/articles/articles.component.ts
+++ src/app/components/dashboard/Editing/articles/articles.component.ts
@@ -1,73 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FlashMessagesService } from 'flash-messages-angular';
 import { Article } from 'src/app/models/models';
 import { ArticleService } from 'src/app/services/Admin/Editing/article.service';
+
+
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
-  id: string = '';
-
-  article: Article = {
-    id: '',
-    contenu: '',
-    payload: undefined,
-    titre: '',
-    image: ''
-  };
-
-  constructor(
-    private _articleService: ArticleService,
-    private router: Router,
-    private route: ActivatedRoute,
-    private _flashmessage: FlashMessagesService
-  ) {}
+export class ArticlesComponent  implements OnInit {
+  constructor(public article: ArticleService) {}
+  articles: Article[] = [];
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id') as string;
-    if (this.id)
-      this._articleService
-        .getArticle(this.id)
-        .subscribe((a) => (this.article = a));
-  }
-
-  save(articleForm: NgForm) {
-    if (articleForm.valid) {
-      if (this.id) {
-        this._articleService.updateArticle(articleForm.value, this.id);
-        this._flashmessage.show('Article Mis à jour', {
-          cssClass:'alert alert-primary',
-          setTimeout: 4000,
-        });
-      } else {
-        this._articleService.addArticle(articleForm.value);
-        this._flashmessage.show('Nouvel article créé', {
-          cssClass: 'alert alert-primary',
-          setTimeout: 4000,
-        });
-      }
-      this.router.navigate(['/']);
-    } else {
-      this._flashmessage.show('Il y a des erreurs !', {
-        cssClass: 'alert alert-danger',
-        setTimeout: 4000,
-      });
-    }
-  }
-  delete(){
-    if (confirm('Etes vous certain de vouloir suprrimer ?')) {
-      this._articleService.deleteArticle(this.id);
-      this._flashmessage.show('Article supprimer', {
-        cssClass: 'alert alert-danger',
-        setTimeout: 4000,
-      });
-      this.router.navigate(['/']);
-    }
+    this.article.getArticles().subscribe((a: Article[]) => {
+      this.articles = a;
+    });
+    console.log(this.article);
   }
 }
 

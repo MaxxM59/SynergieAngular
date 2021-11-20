234a75ecfc24ccc6e582a5e5e7fb57090c54a89d
Max <87809496+MaxxM59@users.noreply.github.com>
2021-11-20 16:44:38 +0100

    Update nav.component.ts

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Onglet } from 'src/app/models/models';
+import { Onglet, Page } from 'src/app/models/models';
 import { AdminLoginService } from 'src/app/services/Admin/admin-login.service';
 import { OngletsService } from 'src/app/services/Admin/Editing/onglets.service';
+import { PagesService } from 'src/app/services/Admin/Editing/pages.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,9 +11,11 @@ import { OngletsService } from 'src/app/services/Admin/Editing/onglets.service';
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  pages: Page[] = [];
   constructor(
     public ongletservice: OngletsService,
-    private admin: AdminLoginService
+    private admin: AdminLoginService,
+    private pagesservice: PagesService
   ) {}
 
   onglets: Onglet[] = [];
@@ -21,6 +24,7 @@ export class NavComponent implements OnInit {
   solo: any;
   check = 'dd.dossier=== d.titre';
   auth = this.admin.auth;
+
   // TRI DES ONGLETS
   tri(tab: Onglet[]) {
     // ONGLETS SOLOS
@@ -36,10 +40,29 @@ export class NavComponent implements OnInit {
       (onglet) => onglet.dossier !== 'Aucun' && onglet.dossier !== ''
     );
   }
+  //LINK DES PAGES AUX LIENS
+
+  link() {
+    this.onglets.forEach((o) =>
+      this.pages.forEach((p) => {
+        if (o.titre === p.titre) {
+          o.lien = p.id;
+        }
+      })
+    );
+  }
 
   ngOnInit(): void {
+    //RECUPERE TOUTES LES PAGES
+    this.pagesservice.getPages().subscribe((p: Page[]) => {
+      this.pages = p;
+    });
+    //RECUPERE TOUS LES ONGLETS
     this.ongletservice.getOnglets().subscribe((o: Onglet[]) => {
       this.onglets = o;
+      //LINK ONGLETS<--->PAGES
+      this.link();
+      //TRI DES ONGLETS POUR AFFICHAGE NAV
       this.tri(this.onglets);
     });
   }

a87046ad24d820c83014bf5976febdd3830ca8ba
Max <87809496+MaxxM59@users.noreply.github.com>
2021-11-24 15:35:35 +0100

    up

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { NgSwitchDefault } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Article } from 'src/app/models/models';
@@ -13,11 +14,15 @@ export class HomeComponent implements OnInit {
   constructor(public article: ArticleService) {}
 
   articles: Article[] = [];
-
+  active: any;
+  items: any;
   ngOnInit(): void {
     this.article.getArticles().subscribe((a: Article[]) => {
       this.articles = a;
+      this.active = this.articles[0];
+      this.items = this.articles.slice(1, 3);
     });
+
     console.log(this.articles);
   }
 }

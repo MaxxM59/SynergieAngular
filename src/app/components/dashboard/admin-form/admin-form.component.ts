978641d1146f59b31ce94c7bf864338e45bb9525
Max <87809496+MaxxM59@users.noreply.github.com>
2022-03-01 18:40:29 +0100

    Fixed form before deleting fireauth

--- src/app/components/dashboard/admin-form/admin-form.component.ts
+++ src/app/components/dashboard/admin-form/admin-form.component.ts
@@ -4,7 +4,6 @@ import {
   Validators,
   FormGroup,
   FormControl,
-  NgForm,
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Admin } from 'src/app/models/models';
@@ -19,7 +18,7 @@ export class AdminFormComponent implements OnInit {
   isHide = true;
   formValid = false;
   admins: Admin[] = [];
-
+  error: boolean = false;
   constructor(
     private router: Router,
     private admin: AdminLoginService,
@@ -35,7 +34,7 @@ export class AdminFormComponent implements OnInit {
     ]),
   });
   // RECUPERE MAIL
-  get email(): AbstractControl | null {
+  get mail(): AbstractControl | null {
     return this.loginForm.get('mail');
   }
   // RECUPERE PASSWORD
@@ -46,18 +45,21 @@ export class AdminFormComponent implements OnInit {
   // FONCTION CONNEXION
   Connexion() {
     if (this.loginForm.valid) {
-      this.formValid = true;
-
-      for (let i = 0; i < this.admins.length; i++) {
-        if (this.loginForm.value.mail === this.admins[i].mail) {
+      for (let i = 0; i < this.admins.length - 1; i++) {
+        if (
+          this.admins[i].mail.includes(this.loginForm.value.mail) &&
+          this.admins[i].password.includes(this.loginForm.value.password)
+        ) {
           this.admin.auth = true;
           this.router.navigate(['dashboard']);
         } else {
-          this.formValid = false;
-          this.notif.showNotification(
-            `Une erreur s'est produite, il y a une erreur dans l'email ou le mot de passe.`
-          );
+          this.error = true;
         }
+      }
+      if (this.error) {
+        this.notif.showNotification(
+          `Une erreur s'est produite, il y a une erreur dans l'email ou le mot de passe.`
+        );
       }
     }
   }

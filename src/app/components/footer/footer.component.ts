20978362275f38517e9882d51195014585dc5035
Max <87809496+MaxxM59@users.noreply.github.com>
2022-05-17 14:45:23 +0200

    Fixed navbar

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,25 +3,31 @@ import { Router } from '@angular/router';
 import { GotoLinkServiceService } from 'src/app/services/Admin/goto-link-service.service';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  // VARIABLES
+    // VARIABLES
 
-  contact: string = '';
+    contact: string = '';
 
-  constructor(public admin: GotoLinkServiceService, private router: Router) {}
+    constructor(public admin: GotoLinkServiceService, private router: Router) {}
 
-  ngOnInit(): void {
-    if (
-      this.router.url.startsWith('localhost:4200/page/') ||
-      this.router.url.startsWith('localhost:4200/contactform')
-    ) {
-      this.contact = '../contactform';
-    } else {
-      this.contact = '/contactform';
+    ngOnInit(): void {
+        if (
+            this.router.url.startsWith('localhost:4200/page/') ||
+            this.router.url.startsWith('localhost:4200/contactform') ||
+            this.router.url.startsWith(
+                'https://synergieangular.firebaseapp.com/page'
+            ) ||
+            this.router.url.startsWith(
+                'https://synergieangular.firebaseapp.com/contactform'
+            )
+        ) {
+            this.contact = '../contactform';
+        } else {
+            this.contact = '/contactform';
+        }
     }
-  }
 }
